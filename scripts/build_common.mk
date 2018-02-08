01ca004cea71b64bae9c0e9b27640d84c7acbc5b
AndreaRigoni <andrea.rigoni@pd.infn.it>
2018-02-08 17:20:05 +0100

    add yocto

--- scripts/build_common.mk
+++ scripts/build_common.mk
@@ -22,17 +22,15 @@
 
 MAKE_PROCESS  ?= $(shell grep -c ^processor /proc/cpuinfo)
 DOWNLOAD_DIR  ?= $(top_builddir)/download
-
+DOWNLOADS     ?=
 
 ## ////////////////////////////////////////////////////////////////////////// ##
 ## ///  DOWNLOAD  /////////////////////////////////////////////////////////// ##
 ## ////////////////////////////////////////////////////////////////////////// ##
 
-_flt ?= $(subst ' ',_,$(subst .,_,$1))
-
 define dl__download_tar =
  $(info "Downloading tar file: $1")
- mkdir -p ${DOWNLOAD_DIR} $2; \
+ $(MKDIR_P) ${DOWNLOAD_DIR} $2; \
  _tar=${DOWNLOAD_DIR}/$$(echo $1 | sed -e 's|.*/||'); \
  test -f $$_tar || curl -SL $1 > $$_tar; \
  _wcl=$$(tar -tf $$_tar | sed -e 's|/.*||' | uniq | wc -l); \
@@ -45,26 +43,44 @@ endef
 
 define dl__download_git =
  $(info "Downloading git repo: $1")
- git clone $1 $2 $(if $(value $(call _flt,$2)_BRANCH),-b $(value $(call _flt,$2)_BRANCH))
+ git clone $1 $2 $(if $3,-b $3)
 endef
 
 define dl__download_generic =
  $(info "Downloading file: $1")
- mkdir -p ${DOWNLOAD_DIR}; \
+ $(MKDIR_P) ${DOWNLOAD_DIR}; \
  _f=${DOWNLOAD_DIR}/$$(echo $1 | sed -e 's|.*/||'); \
  test -f $$_f || curl -SL $1 > $$_f; \
  $(LN_S) $$_f $2;
 endef
 
 dl__tar_ext = %.tar %.tar.gz %.tar.xz %.tar.bz %.tar.bz2
-dl__git_ext = %.git
+dl__git_ext = git://% %.git
 
+define dl__dir =
+$(if $(filter-out $1,$($1_DIR)),
+$($1_DIR): $$($1_DEPS)
+	@ $(MAKE) $(AM_MAKEFLAGS) download NAME=$1
+)
+endef
+$(foreach x,$(DOWNLOADS),$(eval $(call dl__dir,$x)))
+
+
+$(DOWNLOADS): _flt = $(subst ' ',_,$(subst .,_,$1))
 $(DOWNLOADS):
-	@ $(foreach x,$($(call _flt,$@)_URL),\
+	@ $(MAKE) $(AM_MAKEFLAGS) download NAME=$(call _flt,$@)
+
+.PHONY: download
+download: ##@miscellaneous download target in $NAME and $DOWNLOAD_URL
+download: URL     = $(or $($(NAME)_URL),$(DOWNLOAD_URL))
+download: DIR     = $(or $($(NAME)_DIR),$(NAME))
+download: BRANCH := $(or $($(NAME)_BRANCH),$(BRANCH))
+download: $(or $($(NAME)_DEPS), $(DOWNLOAD_DEPS))
+	@ $(foreach x,$(URL),\
 		$(info Download: $x) \
-		$(if $(filter $(dl__tar_ext),$x),$(call dl__download_tar,$x,$@), \
-		$(if $(filter $(dl__git_ext),$x),$(call dl__download_git,$x,$@), \
-		$(call dl__download_generic,$x,$@) ) ) \
+		$(if $(filter $(dl__tar_ext),$x),$(call dl__download_tar,$x,$(DIR)), \
+		$(if $(filter $(dl__git_ext),$x),$(call dl__download_git,$x,$(DIR),$(BRANCH)), \
+		$(call dl__download_generic,$x,$(DIR)) ) ) \
 	   )
 
 
---
+++ scripts/yocto.mk
@@ -0,0 +1,66 @@
+## ////////////////////////////////////////////////////////////////////////// //
+##
+## This file is part of the autoconf-bootstrap project.
+## Copyright 2018 Andrea Rigoni Garola <andrea.rigoni@igi.cnr.it>.
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## ////////////////////////////////////////////////////////////////////////// //
+
+
+################################################################################
+# Yocto download
+################################################################################
+
+if !YOCTO_DOCKERBUILD
+NODOCKERBUILD = yocto%
+endif
+
+if ENABLE_YOCTO
+yocto: ##@yocto download and prepare yocto poky build environment
+YOCTO_DIR ?= $(builddir)/yocto
+
+DOWNLOADS += yocto
+yocto_DIR    := $(YOCTO_DIR)
+yocto_URL    := $(YOCTO_URL)
+yocto_BRANCH := $(YOCTO_GIT_BRANCH)
+
+YOCTO_BUILD = $(YOCTO_DIR)/build
+YOCTO_PYBIN_PATH = $(YOCTO_BUILD)/python-bin
+
+$(YOCTO_PYBIN_PATH):
+	@ $(MKDIR_P) $@
+
+$(YOCTO_PYBIN_PATH)/python: |$(YOCTO_PYBIN_PATH)
+	@ $(LN_S) /usr/bin/python2 $@
+
+$(YOCTO_PYBIN_PATH)/python-config: |$(YOCTO_PYBIN_PATH)
+	@ $(LN_S) /usr/bin/python2-config $@
+
+YOCTO_PY2_LINKS =  \
+   $(YOCTO_PYBIN_PATH)/python \
+   $(YOCTO_PYBIN_PATH)/python-config
+
+yocto-py2-link: ##@yocto build links for python2
+yocto-py2-link: $(YOCTO_DIR) $(YOCTO_PY2_LINKS)
+
+
+yocto-shell: ##@yocto enter oe-init-build-env
+yocto-shell: PATH := $(abs_top_builddir)/$(YOCTO_PYBIN_PATH):$(PATH)
+yocto-shell: PS1  := \\u@\h:yocto \\W]\\$$
+yocto-shell: yocto-py2-link
+	@ cd $(YOCTO_DIR); source ./oe-init-build-env; \
+	  bash -l
+
+endif
